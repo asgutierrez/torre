2f5c8ebfd55f59b2022ed3a7453b7d3090f614a4
asgutierrez <asgutierrez@uninorte.edu.co>
2021-01-23 17:47:20 -0500

    Yarn

--- functions/auth-fetch/auth-fetch.js
+++ functions/auth-fetch/auth-fetch.js
@@ -1,6 +1,6 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 const handler = async function (event, context) {
   if (!context.clientContext && !context.clientContext.identity) {
@@ -10,30 +10,31 @@ const handler = async function (event, context) {
       body: JSON.stringify({
         msg: 'No identity instance detected. Did you enable it?',
       }),
-    }
+    };
   }
-  const { identity, user } = context.clientContext
+  const { identity, user } = context.clientContext;
   try {
-    const response = await fetch('https://torre.bio/api/bios/stevenandresgutierrezpadilla')
+    const username = event.queryStringParameters.search;
+    const response = await fetch(`https://torre.bio/api/bios/${username}`);
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.statusText }
+      return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json()
-    console.log(data)
+    const data = await response.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ identity, user, msg: data.value }),
-    }
+      body: JSON.stringify({ identity, user, msg: data }),
+    };
   } catch (error) {
     // output to netlify function log
-    console.log(error)
+    console.error(error);
     return {
       statusCode: 500,
       // Could be a custom message or object i.e. JSON.stringify(err)
       body: JSON.stringify({ msg: error.message }),
-    }
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
