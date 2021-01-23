4666fe8ae550d47864cd20c88deeb8a52c8997b9
asgutierrez <asgutierrez@uninorte.edu.co>
2021-01-23 15:38:08 -0500

    Envir

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 Vue.use(VueMaterial)
 
 new Vue({
